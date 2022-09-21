be33dd0fc4bf770750f368b73f0fffbc6d2f0cd0
valentinaiori <valentinaiori@live.it>
2022-09-21 16:25:23 +0200

    Add exercise

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>La mia prima app con Vue CLI!</h1>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
